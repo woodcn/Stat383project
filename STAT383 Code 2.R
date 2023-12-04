42c6b3b80afbf8d193c1f61affae47f064fdfba1
woodcn <152556107+woodcn@users.noreply.github.com>
2023-12-04 17:55:36 -0500

    Add files via upload

---
+++ STAT383 Code 2.R
@@ -0,0 +1,4 @@
+cat("\f") # clean the console
+rm(list = ls()) # clean the environment
+prop.test(x=c(259,77), n=c(75694,35177), p = NULL, alternative = "two.sided") #1 is soccer and 2 is lacrosse
+
